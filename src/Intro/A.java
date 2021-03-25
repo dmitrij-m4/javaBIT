78106a937130b4c51beed8d1ad50dae4a15d8771
dmitrij-m4 <79442201+dmitrij-m4@users.noreply.github.com>
2021-03-25 13:31:58 +0200

    sachmatai

--- src/Intro/A.java
+++ src/Intro/A.java
@@ -2,6 +2,19 @@ package Intro;
 
 public class A {
     public static void main(String[] args) {
-    System.out.println("Yes :)");
+
+        int i = 100;
+
+        for (int k = 0 ; k < 64; k++) {
+
+            int j = 2 ;
+
+
+
+            System.out.println(Math.pow(j, k));
+        }
+
+    System.out.println("i = " + (i + 2));
+
 }
 }
