a405fa533c9333edf9d380f9de2dc0aa6936542d
dmitrij-m4 <79442201+dmitrij-m4@users.noreply.github.com>
2021-03-28 13:18:47 +0300

    vyksta

---
+++ src/lt/dmitrij/a.java
@@ -0,0 +1,19 @@
+package lt.dmitrij;
+
+public class a {
+    public static void main(String[] args) {
+
+        int i = 100;
+
+        for (int k = 0; k < 64; k++) {
+
+            int j = 2;
+
+
+            System.out.println(Math.pow(j, k));
+        }
+
+        System.out.println("i = " + (i + 2));
+
+    }
+}
