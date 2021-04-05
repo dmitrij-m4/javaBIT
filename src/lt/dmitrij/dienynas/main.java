a70754f1d5c3aadd36f9843172916f6163d8a4f5
dmitrij-m4 <79442201+dmitrij-m4@users.noreply.github.com>
2021-04-05 20:21:51 +0300

    dienynas spausdinimai

--- src/lt/dmitrij/dienynas/main.java
+++ src/lt/dmitrij/dienynas/main.java
@@ -1,7 +1,25 @@
 package lt.dmitrij.dienynas;
 
 public class main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        class mokinys {
+
+            String vardas;
+            String pavarde;
+            int klase;
+
+            mokinys(){}
+
+            mokinys(String vardas, String pavarde, int klase){
+                this.vardas = vardas;
+                this.pavarde = pavarde;
+                this.klase = klase;
+            }
+        }
+        mokinys[] mokiniai =  {
+            new mokinys("Jonas", "Jonaitis", 1)
+        };
+        System.out.println(mokiniai[0].vardas + " " + mokiniai[0].pavarde + " klase : " + mokiniai[0].klase);
+        }
 
     }
-}
