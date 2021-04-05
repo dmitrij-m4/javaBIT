24ffe9e38435d20f40f46b20850b094508afbd64
dmitrij-m4 <79442201+dmitrij-m4@users.noreply.github.com>
2021-04-05 13:32:21 +0300

    q

--- src/lt/dmitrij/rusiavimas/masyvoRusiavimas.java
+++ src/lt/dmitrij/rusiavimas/masyvoRusiavimas.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class masyvoRusiavimas {
    public static void main (String[] args) {
 
-       int[] masyvas = {1,2,4,8,5, -300, 67,88,654};
+       int[] masyvas = {1,100,-900,89,24,-6};
        System.out.println(Arrays.toString(masyvas));
 
        for (int i = 0; i < masyvas.length; i++ ) {
