8eead20e54e36d78c8d0adfcc7e4fbaf81a78d3e
dmitrij-m4 <79442201+dmitrij-m4@users.noreply.github.com>
2021-04-06 09:56:35 +0300

    dienynas spausdinimai

--- src/lt/dmitrij/dienynas/main.java
+++ src/lt/dmitrij/dienynas/main.java
@@ -17,9 +17,56 @@ public class main {
             }
         }
         mokinys[] mokiniai =  {
-            new mokinys("Jonas", "Jonaitis", 1)
+            new mokinys("Jonas", "Jonaitis", 3),
+            new mokinys("Petras", "Petraitis", 2),
+                new mokinys("Antanas", "Antanauskas", 1),
+                new mokinys("Stasys", "Stasaitis", 1),
+                new mokinys("Algis", "Stasaitis", 1),
+
         };
-        System.out.println(mokiniai[0].vardas + " " + mokiniai[0].pavarde + " klase : " + mokiniai[0].klase);
+
+        for (int i = 0; i < mokiniai.length; i++) {
+            System.out.println(mokiniai[i].vardas + " " + mokiniai[i].pavarde + " klase : " + mokiniai[i].klase);
+            for (int k = 0; k < mokiniai.length; k++){
+//                System.out.println(mokiniai[k].vardas + " " + mokiniai[k].pavarde + " klase : " + mokiniai[k].klase);
+                if (mokiniai[i].klase < mokiniai[k].klase) {
+                   int tarpinis = mokiniai[i].klase;
+                    mokiniai[i].klase = mokiniai[k].klase;
+                    mokiniai[k].klase = tarpinis;
+                    String tarpinis1 = mokiniai[i].vardas;
+                    mokiniai[i].vardas = mokiniai[k].vardas;
+                    mokiniai[k].vardas = tarpinis1;
+                    String tarpinis2 = mokiniai[i].pavarde;
+                    mokiniai[i].pavarde = mokiniai[k].pavarde;
+                    mokiniai[k].pavarde = tarpinis2;
+                }
+                else if ((mokiniai[i].klase == mokiniai[k].klase) && ((mokiniai[i].pavarde).compareToIgnoreCase(mokiniai[k].pavarde) < 0) ) {
+                    String tarpinis1 = mokiniai[i].vardas;
+                    mokiniai[i].vardas = mokiniai[k].vardas;
+                    mokiniai[k].vardas = tarpinis1;
+                    String tarpinis2 = mokiniai[i].pavarde;
+                    mokiniai[i].pavarde = mokiniai[k].pavarde;
+                    mokiniai[k].pavarde = tarpinis2;
+
+                }
+
+                else if ((mokiniai[i].klase == mokiniai[k].klase)
+                        && ((mokiniai[i].pavarde).compareToIgnoreCase(mokiniai[k].pavarde) >= 0)
+                        && ((mokiniai[i].vardas).compareToIgnoreCase(mokiniai[k].vardas)) < 0) {
+                    String tarpinis1 = mokiniai[i].vardas;
+                    mokiniai[i].vardas = mokiniai[k].vardas;
+                    mokiniai[k].vardas = tarpinis1;
+
+
+                }
+
+            }
+
         }
+        for (int i = 0; i < mokiniai.length; i++) {
+
+            System.out.println(mokiniai[i].vardas + " " + mokiniai[i].pavarde + " klase : " + mokiniai[i].klase);
+        }
+    }
 
     }
